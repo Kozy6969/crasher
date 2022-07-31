52e32fea1b61b3eadea38437a7569978a5c4a357
Kozy6969 <107891583+Kozy6969@users.noreply.github.com>
2022-07-31 22:25:25 +0300

    Update and rename kozyahaub.lua to kozyahaub1.lua

---
+++ kozyahaub1.lua
@@ -0,0 +1,2 @@
+cht("h  \n\n\n\n\n\n\ \n\n\n\n\n\n\n\ \n\n\n\n\n\n\ \n\n\n\n\n\n\ \n\n\n\n\n\n\n\ \n\n\n\ \n \n \n \n \n   ⚪                                                                                       \n\n\n\n\ ")
+cht("h  \n\n\n\n\n\n\ \n\n\n\n\n\n\n\ \n\n\n\n\n\n\ \n\n\n\n\n\n\ \n\n\n\n\n\n\n\ \n\n\  ⚪    /                                                                                \n\n\n\n\ ")
